25556613f5681e726f6356994044e81c2c6e3ebc
Fabio Petrone <fa610movic90@gmail.com>
2021-01-21 13:46:10 +0100

    array di oggetti icone

--- js/script.js
+++ js/script.js
@@ -1,1 +1,107 @@
+// @here
+// BUM.
+// Esercizio di oggi Js Icons Es6
+// repo/cartella js-es6-icons
+// Milestone 1:
+// - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
+// - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 
+
+let icons_list = [
+  {
+    name : 'cat',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'crow',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'dog',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'dove',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'dragon',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'horse',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'hippo',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'fish',
+    prefix : 'fa-',
+    type : 'animal',
+    family : 'fas',
+  },
+  {
+    name : 'carrot',
+    prefix : 'fa-',
+    type : 'vegetable',
+    family : 'fas',
+  },
+  {
+    name : 'apple-alt',
+    prefix : 'fa-',
+    type : 'vegetable',
+    family : 'fas',
+  },
+  {
+    name : 'lemon',
+    prefix : 'fa-',
+    type : 'vegetable',
+    family : 'fas',
+  },
+  {
+    name : 'pepper-hot',
+    prefix : 'fa-',
+    type : 'vegetable',
+    family : 'fas',
+  },
+  {
+    name : 'user-astronaut',
+    prefix : 'fa-',
+    type : 'user',
+    family : 'fas',
+  },
+  {
+    name : 'user-graduate',
+    prefix : 'fa-',
+    type : 'user',
+    family : 'fas',
+  },
+  {
+    name : 'user-ninja',
+    prefix : 'fa-',
+    type : 'user',
+    family : 'fas',
+  },
+  {
+    name : 'user-secret',
+    prefix : 'fa-',
+    type : 'user',
+    family : 'fas',
+  },
+];
