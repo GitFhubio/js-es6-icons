c3686bbe2e57b8420b5bc36fcbd18831b4c7aaf0
Fabio Petrone <fa610movic90@gmail.com>
2021-01-21 13:59:21 +0100

    array di tipi e di colori

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 // BUM.
 // Esercizio di oggi Js Icons Es6
 // repo/cartella js-es6-icons
+
 // Milestone 1:
 // - Definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
 // - Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
-
 
 let icons_list = [
   {
@@ -105,3 +105,29 @@ let icons_list = [
     family : 'fas',
   },
 ];
+
+const iconsContainer=document.getElementById('icons-container');
+
+icons_list.forEach((element) => {
+iconsContainer.innerHTML+=`<div><i class="${element.family} ${element.prefix}${element.name}"></i><span>${element.name}</span></div>`
+});
+
+// Milestone 2:
+// - Definire un array di colori e associare ad ogni tipo di icona un colore.
+// - Visualizzare le icone di colore diverso in base al tipo.
+
+
+const colors=['blue','orange','purple']
+// i colori sono 3 perché i tipi sono tre
+const types_list=[];
+icons_list.forEach((item) => {
+const {type}=item;
+// oppure senza destrutturazione const type=item.type;
+if (!types_list.includes(type))
+{
+ types_list.push(type);
+}
+
+});
+// ora ho array di colori e array di tipi
+console.log(types_list);
